2018e79219c1b2f504cb5e2f38f09603b5164b01
rupeshmahajan711 <rupeshmahajan711@gmail.com>
2024-12-21 09:41:16 +0530

    added logout page test case

--- src/main/java/com/testingshashtra/pages/HomePage.java
+++ src/main/java/com/testingshashtra/pages/HomePage.java
@@ -18,11 +18,18 @@ public class HomePage {
 
 	@FindBy(css = "a[href='/web/index.php/pim/updatePassword']")
 	private static WebElement changePassword;
-	
+
 	@FindBy(css = "h6.oxd-topbar-header-breadcrumb-module")
 	private static WebElement checkForWebElement;
+
+	@FindBy(css = "a[href='/web/index.php/auth/logout']")
+	private static WebElement logoutButton;
 	
+	@FindBy(css = "input[name='username']")
+	private static WebElement checkForUsername;
+
 	public static void clickDropDownMenu() {
+		WaitFor.visibilityOfElement(dropDownMenu);
 		Keywords.clickOnWebElement(dropDownMenu);
 	}
 
@@ -34,5 +41,14 @@ public class HomePage {
 	public static boolean checkForWebElementPresence() {
 		WaitFor.visibilityOfElement(checkForWebElement);
 		return Keywords.elementIsDisplayed(checkForWebElement);
+	}
+
+	public static void clickLogoutButton() {
+		Keywords.clickOnWebElement(logoutButton);
+	}
+	
+	public static boolean checkForPresenceOfUsername() {
+		WaitFor.visibilityOfElement(checkForUsername);
+		return Keywords.elementIsDisplayed(checkForUsername);
 	}
 }
